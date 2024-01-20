9a284fcbc2f64585902b030439623d19ce51d1e4
Mihael Marović <www.mihael@gmail.com>
2024-01-20 19:12:46 +0100

    start parser

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace parsejsondotnet2;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World 2!");
+        string json;
+        JObject array;
+        using (StreamReader r = new StreamReader("./ConsoleApp1/jsonfile/SabotageSAS.mup"))
+        {
+            json = r.ReadToEnd();
+            array = (JObject)JsonConvert.DeserializeObject(json);
+        }
+        Console.WriteLine("{0}", array.Count);
+    }
+}
