4a78771d30ad9c396b725c4bd7be4495a0080207
Mihael Marović <www.mihael@gmail.com>
2024-01-23 17:57:24 +0100

    basic functionality (not fully developed)

--- parsepython/parse.py
+++ parsepython/parse.py
@@ -1,16 +1,25 @@
 import json
 
-str1 = "Directly send maniuplated meassurement values to IED with process bus rogue device"
-str2 = "Directly send maniuplated meassurement values to IED from process bus switch"
+str1 = "Directly send manipulated measurement values to IED with process bus rogue device"
+str2 = "Directly send manipulated measurement values to IED from process bus switch"
 
 lst = []
 lst.append(str1)
 lst.append(str2)
-print (lst)
+#print (lst)
 
 f = open ('jsonfile/SabotageSAS.mup')
 data = json.load(f)
-print (data["ideas"]['1']['ideas']['21']['ideas']['3']["title"])
 
+print (data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"]["1"]["title"])
+print (data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"]["3"]["title"])
+
+"""
+data[")ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"].pop("1")
+data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"].pop("3")
+print(data)
+"""
 #samo backtrackam za ove druge koje su analogna shema kao "Directly send a command to circuit breaker"
 #i onda ako mogu izbrisati gg
+#we found them
+#now we have to find out, not only how to delte them, but also their children
