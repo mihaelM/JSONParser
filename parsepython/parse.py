ed1ea230515b65d8c22b4badee12bee8076de67f
Mihael Marović <www.mihael@gmail.com>
2024-01-27 20:25:08 +0100

    using only python, rewriting and testing SMV protection

--- parsepython/parse.py
+++ parsepython/parse.py
@@ -1,25 +1,23 @@
 import json
+# "Directly send manipulated measurement values to IED with process bus rogue device"
+# <-> data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"]["1"]["title"]
+# "Directly send manipulated measurement values to IED from process bus switch"
+# <-> data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"]["3"]["title"]
 
-str1 = "Directly send manipulated measurement values to IED with process bus rogue device"
-str2 = "Directly send manipulated measurement values to IED from process bus switch"
+#tested and seems to be working
+def deleteNodesAffectedBySMVProtection(data):
+    data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"].pop("1")
+    data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"].pop("3")
 
-lst = []
-lst.append(str1)
-lst.append(str2)
-#print (lst)
+def outputData(data):
+    f = open('output/SabotageSAS_withProtection.mup', 'w')
+    json.dump(data, f)
+    f.close()
 
-f = open ('jsonfile/SabotageSAS.mup')
+f = open('jsonfile/SabotageSAS.mup', 'r')
 data = json.load(f)
 
-print (data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"]["1"]["title"])
-print (data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"]["3"]["title"])
+deleteNodesAffectedBySMVProtection(data)
+outputData(data)
 
-"""
-data[")ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"].pop("1")
-data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"].pop("3")
-print(data)
-"""
-#samo backtrackam za ove druge koje su analogna shema kao "Directly send a command to circuit breaker"
-#i onda ako mogu izbrisati gg
-#we found them
-#now we have to find out, not only how to delte them, but also their children
+f.close()
