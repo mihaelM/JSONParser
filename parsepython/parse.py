e239df696fb876b917662b9b8412dfafbda1693a
Mihael Marović <www.mihael@gmail.com>
2024-01-30 19:17:08 +0100

    improving parser script

--- parsepython/parse.py
+++ parsepython/parse.py
@@ -3,11 +3,27 @@ import json
 # <-> data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"]["1"]["title"]
 # "Directly send manipulated measurement values to IED from process bus switch"
 # <-> data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"]["3"]["title"]
+# "MITM attack which manipulates measurement values sent to IED" (3_3)
+# <->
 
 #tested and seems to be working
 def deleteNodesAffectedBySMVProtection(data):
     data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"].pop("1")
     data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"].pop("3")
+
+# "Directly send manipulated measurement values to SCADA with station bus rogue device"
+# <-> data["ideas"]["1"]["ideas"]["0.5625"]["ideas"]["2"]["ideas"]["1"]["title"]
+# "Directly send manipulated measurement values to the SCADA from corporate WAN"
+# <->
+# "Directly send manipulated measurement values to the SCADA from station bus switch"
+# <->
+# "MITM attack which manipulates measurement values sent to the SCADA" (3_4)
+# <->
+
+
+
+def deleteNodesAffecteByMMSProtection(data):
+    pass
 
 def outputData(data):
     f = open('output/SabotageSAS_withProtection.mup', 'w')
@@ -17,7 +33,8 @@ def outputData(data):
 f = open('jsonfile/SabotageSAS.mup', 'r')
 data = json.load(f)
 
-deleteNodesAffectedBySMVProtection(data)
-outputData(data)
+#deleteNodesAffectedBySMVProtection(data)
+#outputData(data)
+print (data["ideas"]["1"]["ideas"]["0.5625"]["ideas"]["2"]["ideas"]["1"]["title"])
 
 f.close()
