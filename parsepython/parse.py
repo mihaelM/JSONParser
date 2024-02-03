7b4370294680ce81444d090fdfbd6b2f2d9a9a8f
Mihael Marović <www.mihael@gmail.com>
2024-02-03 15:50:55 +0100

    improving code quality

--- parsepython/parse.py
+++ parsepython/parse.py
@@ -5,12 +5,11 @@ import json
 # <-> data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"]["3"]["title"]
 # "MITM attack which manipulates measurement values sent to IED" (3_3)
 # <-> data["ideas"]["1"]["ideas"]["0.5625"]["ideas"]["3"]["title"]
-
-#tested and seems to be working
 def deleteNodesAffectedBySMVProtection(data):
     data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"].pop("1")
     data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"].pop("3")
     data["ideas"]["1"]["ideas"]["0.5625"]["ideas"].pop("3")
+
 
 # "Directly send manipulated measurement values to SCADA with station bus rogue device"
 # <-> data["ideas"]["1"]["ideas"]["0.5625"]["ideas"]["2"]["ideas"]["1"]["title"]
@@ -20,7 +19,6 @@ def deleteNodesAffectedBySMVProtection(data):
 # <-> data["ideas"]["1"]["ideas"]["0.5625"]["ideas"]["2"]["ideas"]["5"]["title"]
 # "MITM attack which manipulates measurement values sent to the SCADA" (3_4)
 # <-> data["ideas"]["1"]["ideas"]["0.5625"]["ideas"]["4"]["title"]
-
 def deleteNodesAffectedByOnlyMMSProtection(data):
     data["ideas"]["1"]["ideas"]["0.5625"]["ideas"]["2"]["ideas"].pop("1")
     data["ideas"]["1"]["ideas"]["0.5625"]["ideas"]["2"]["ideas"].pop("3")
@@ -36,7 +34,6 @@ def deleteNodesAffectedByOnlyMMSProtection(data):
 # <-> data["ideas"]["1"]["ideas"]["0.75"]["ideas"]["1"]["ideas"]["7"]["title"]
 #MITM attack which sends a command to IED
 # <-> data["ideas"]["1"]["ideas"]["0.75"]["ideas"]["3"]["title"]
-
 def deleteNodesAffectedByGOOSEandMMSProtection(data):
     data["ideas"]["1"]["ideas"]["0.75"]["ideas"]["1"]["ideas"].pop("1")
     data["ideas"]["1"]["ideas"]["0.75"]["ideas"]["1"]["ideas"].pop("2")
@@ -50,20 +47,20 @@ def deleteNodesAffectedByGOOSEandMMSProtection(data):
 # <-> data["ideas"]["1"]["ideas"]["0.75"]["ideas"]["2"]["ideas"]["5"]["title"]
 #MTIM attack which sends a command to circuit breaker IED
 # <-> data["ideas"]["1"]["ideas"]["0.75"]["ideas"]["4"]["title"]
-
 def deleteNodesAffectedByOnlyGOOSEProtection(data):
     data["ideas"]["1"]["ideas"]["0.75"]["ideas"]["2"]["ideas"].pop("1")
     data["ideas"]["1"]["ideas"]["0.75"]["ideas"]["2"]["ideas"].pop("5")
     data["ideas"]["1"]["ideas"]["0.75"]["ideas"].pop("4")
+
 
 def outputData(data, whichProtection):
     f = open('output/SabotageSAS_withProtection{}.mup'.format(whichProtection), 'w')
     json.dump(data, f)
     f.close()
 
+
 #f = open('jsonfile/SabotageSAS.mup', 'r')
 #data = json.load(f)
-
 #deleteNodesAffectedBySMVProtection(data)
 #outputData(data, "SMV")
 #deleteNodesAffectedByOnlyMMSProtection(data)
@@ -72,5 +69,4 @@ def outputData(data, whichProtection):
 #outputData(data, "GOOSEandMMS")
 #deleteNodesAffectedByOnlyGOOSEProtection(data)
 #outputData(data, "OnlyGOOSE")
-
 #f.close()
