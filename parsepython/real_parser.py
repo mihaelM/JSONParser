9d4c6394aaa7db302520051cee3c64aa406c4003
Mihael Marović <www.mihael@gmail.com>
2024-02-10 18:11:40 +0100

    parser basically working

--- parsepython/real_parser.py
+++ parsepython/real_parser.py
@@ -3,6 +3,8 @@ import json
 #for a shape I need a more representative looking file
 
 #we can solve this without shapes for start
+#good, we add shapes and that is it
+
 def parseJson(data):
     resultingDict = {}
     for key in data["ideas"]:
@@ -15,12 +17,31 @@ def parseJson(data):
             pass
         resultingDict[title] = {}
         resultingDict[title]["position"] = position
-        resultingDict[title]["shape"] = "TODO"
+
+        shape = None
+
+        try:
+            backgroundColor = currDict["attr"]["style"]["backgroundColor"]
+            textColor = currDict["attr"]["style"]["text"]["color"]
+
+            if backgroundColor == "#000000" and textColor == "#FFFFFF":
+                shape = "rhombus"
+
+            if backgroundColor == "#E0E0E0" and textColor == "#4F4F4F":
+                shape = "circle"
+
+            if backgroundColor == "#FFFFFF" and textColor == "#000000":
+                shape = "hexagon"
+
+        except:
+            pass
+
+        resultingDict[title]["shape"] = shape
         if "ideas" in currDict:
             resultingDict[title]["children"] = parseJson(currDict)
     return resultingDict
 
-f = open('mockfile/Kifla.mup', 'r')
+f = open('jsonfile/SabotageSAS.mup', 'r')
 data = json.load(f)
 
 #dobro, čini se da mi od ovih vršnih podataka ne treba ništa
