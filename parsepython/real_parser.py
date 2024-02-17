b418c060cdf634059fc13294d061a939cfa08559
Mihael Marović <www.mihael@gmail.com>
2024-02-17 11:21:51 +0100

    significant step further in cutting and putting protections within tree

--- parsepython/real_parser.py
+++ parsepython/real_parser.py
@@ -1,10 +1,5 @@
 import json
 
-#for a shape I need a more representative looking file
-
-#we can solve this without shapes for start
-#good, we add shapes and that is it
-
 def parseJson(data):
     resultingDict = {}
     for key in data["ideas"]:
@@ -17,9 +12,7 @@ def parseJson(data):
             pass
         resultingDict[title] = {}
         resultingDict[title]["position"] = position
-
         shape = None
-
         try:
             backgroundColor = currDict["attr"]["style"]["backgroundColor"]
             textColor = currDict["attr"]["style"]["text"]["color"]
@@ -32,14 +25,17 @@ def parseJson(data):
 
             if backgroundColor == "#FFFFFF" and textColor == "#000000":
                 shape = "hexagon"
-
         except:
             pass
+
+        if shape == None:
+            shape = "circle" #seems to be that default is gray combination (since not black-white)
 
         resultingDict[title]["shape"] = shape
         if "ideas" in currDict:
             resultingDict[title]["children"] = parseJson(currDict)
     return resultingDict
+
 
 # "Directly send manipulated measurement values to IED with process bus rogue device"
 # "Directly send manipulated measurement values to IED from process bus switch"
@@ -48,18 +44,76 @@ def deleteNodesAffectedBySMVProtectionOurTree(resDict):
     resDict["Sabotage IEC 61850 SAS"]["children"]["Manipulate measurement values"]["children"]["Directly send manipulated measurement values to IED"]["children"].pop("Directly send manipulated measurement values to IED with process bus rogue device")
     resDict["Sabotage IEC 61850 SAS"]["children"]["Manipulate measurement values"]["children"]["Directly send manipulated measurement values to IED"]["children"].pop("Directly send manipulated measurement values to IED from process bus switch")
     resDict["Sabotage IEC 61850 SAS"]["children"]["Manipulate measurement values"]["children"].pop("MITM attack which manipulates measurement values sent to IED")
-
-    #data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"].pop("1")
-    #data["ideas"]['1']['ideas']["0.5625"]["ideas"]["1"]["ideas"].pop("3")
-    #data["ideas"]["1"]["ideas"]["0.5625"]["ideas"].pop("3")
     #replace measuerment with measurement
+
+
+# "Directly send manipulated measurement values to the SCADA with station bus rogue device"
+# "Directly send manipulated measurement values to the SCADA from corporate WAN"
+# "Directly send manipulated measurement values to the SCADA from station bus switch"
+# "MITM attack which manipulates measurement values sent to the SCADA" (3_4)
+def deleteNodesAffectedByOnlyMMSProtectionOurTree(resDict):
+    resDict["Sabotage IEC 61850 SAS"]["children"]["Manipulate measurement values"]["children"]["Directly send manipulated measurement values to the SCADA"]["children"].pop("Directly send manipulated measurement values to the SCADA with station bus rogue device")
+    resDict["Sabotage IEC 61850 SAS"]["children"]["Manipulate measurement values"]["children"]["Directly send manipulated measurement values to the SCADA"]["children"].pop( "Directly send manipulated measurement values to the SCADA from corporate WAN")
+    resDict["Sabotage IEC 61850 SAS"]["children"]["Manipulate measurement values"]["children"]["Directly send manipulated measurement values to the SCADA"]["children"].pop( "Directly send manipulated measurement values to the SCADA from station bus switch")
+    resDict["Sabotage IEC 61850 SAS"]["children"]["Manipulate measurement values"]["children"].pop("MITM attack which manipulates measurement values sent to the SCADA")
+
+
+#Directly send a command to IED with station bus rogue device
+#Directly send a command to IED from corporate WAN
+#Directly send a command to IED from station bus switch
+#MITM attack which sends a command to IED
+def deleteNodesAffectedByGOOSEandMMSProtectionOurTree(resDict):
+    resDict["Sabotage IEC 61850 SAS"]["children"]["Send a command to control element"]["children"]["Directly send a command to IED"]["children"].pop("Directly send a command to IED with station bus rogue device")
+    resDict["Sabotage IEC 61850 SAS"]["children"]["Send a command to control element"]["children"]["Directly send a command to IED"]["children"].pop("Directly send a command to IED from corporate WAN")
+    resDict["Sabotage IEC 61850 SAS"]["children"]["Send a command to control element"]["children"]["Directly send a command to IED"]["children"].pop("Directly send a command to IED from station bus switch")
+    resDict["Sabotage IEC 61850 SAS"]["children"]["Send a command to control element"]["children"].pop("MITM attack which sends a command to IED")
+
+
+#Directly send a command to circuit breaker IED with process bus rogue device
+#Directly send a command to circuit breaker IED from process bus switch
+#MITM attack which sends a command to circuit breaker IED
+def deleteNodesAffectedByOnlyGOOSEProtection(resDict):
+    resDict["Sabotage IEC 61850 SAS"]["children"]["Send a command to control element"]["children"][ "Directly send a command to circuit breaker IED"]["children"].pop( "Directly send a command to circuit breaker IED with process bus rogue device")
+    resDict["Sabotage IEC 61850 SAS"]["children"]["Send a command to control element"]["children"][ "Directly send a command to circuit breaker IED"]["children"].pop( "Directly send a command to circuit breaker IED from process bus switch")
+    resDict["Sabotage IEC 61850 SAS"]["children"]["Send a command to control element"]["children"].pop("MITM attack which sends a command to circuit breaker IED")
+
+
+
+def includeProtectionRecursively(relevantDict, protectionName):
+    relevantDict["protection"] = protectionName
+    if "children" in relevantDict: #even though there is an option for empty children, it seems that sometimes there is no children, also maybe there is a problem with AND
+        for child in relevantDict["children"]:
+            includeProtectionRecursively(relevantDict["children"][child], protectionName)
+
+
+def embedSMVProtectionOurTree(resDict):
+    protectionName = "protect_SMV_with_MAC"
+    relevantSubTree1 = resDict["Sabotage IEC 61850 SAS"]["children"]["Manipulate measurement values"]["children"]["Directly send manipulated measurement values to IED"]["children"]["Directly send manipulated measurement values to IED with process bus rogue device"]
+    includeProtectionRecursively(relevantSubTree1, protectionName)
+
+    relevantSubTree2 = resDict["Sabotage IEC 61850 SAS"]["children"]["Manipulate measurement values"]["children"]["Directly send manipulated measurement values to IED"]["children"]["Directly send manipulated measurement values to IED from process bus switch"]
+    includeProtectionRecursively(relevantSubTree2, protectionName)
+
+    relevantSubTree3 = resDict["Sabotage IEC 61850 SAS"]["children"]["Manipulate measurement values"]["children"]["MITM attack which manipulates measurement values sent to IED"]
+    includeProtectionRecursively(relevantSubTree3, protectionName)
+
 
 f = open('jsonfile/SabotageSAS.mup', 'r')
 data = json.load(f)
 
 #dobro, čini se da mi od ovih vršnih podataka ne treba ništa
 resDict = parseJson(data)
+#print (resDict)
+
+#deleteNodesAffectedBySMVProtectionOurTree(resDict)
+#deleteNodesAffectedByOnlyMMSProtectionOurTree(resDict)
+#deleteNodesAffectedByGOOSEandMMSProtectionOurTree(resDict)
+#deleteNodesAffectedByOnlyGOOSEProtection(resDict)
+
+embedSMVProtectionOurTree(resDict)
 print (resDict)
 
-deleteNodesAffectedBySMVProtectionOurTree(resDict)
-print (resDict)
+#the next task currently is try to put protections within the tree itself
+
+
+
